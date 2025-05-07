ced506159d899501fd2299f37985754c2bbcc4e2
duisterethomas <duisterethomas@gmail.com>
2025-05-07 17:17:31 +0200

    Update gradle and dependencies

--- src/main/java/nl/duisterethomas/pancakemod/PancakeMod.java
+++ src/main/java/nl/duisterethomas/pancakemod/PancakeMod.java
@@ -7,11 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PancakeMod implements ModInitializer {
+	public static final String MOD_ID = "pancakemod";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "pancakemod";
-    public static final Logger LOGGER = LoggerFactory.getLogger("PancakeMod");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
